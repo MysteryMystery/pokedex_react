35155e88d0a0efea76f85f2e0de505873981ac7e
MysteryMystery <jamesattfield1990@gmail.com>
2021-06-01 12:20:41 +0100

    configured for dynamic url

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import "./css/browse.css"
 import Browse from "./components/Browse";
 import Show from "./components/Show";
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-      <Router basename={"/pokedex_react"}>
+      <BrowserRouter basename={"/pokedex_react"}>
           <div className="App h-screen bg-black text-white overflow-auto">
              <Switch>
                  <Route exact path="/" component={ Browse }/>
                  <Route path="/show/:pokemon" component={Show}/>
              </Switch>
           </div>
-      </Router>
+      </BrowserRouter>
   );
 }
 
