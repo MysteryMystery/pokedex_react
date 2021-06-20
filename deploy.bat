e3a51887d14ee03a1a05cbdd51d2a2a480473ba9
MysteryMystery <jamesattfield1990@gmail.com>
2021-06-20 14:05:46 +0100

    Test deployment

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,14 @@
-git subtree push --prefix build origin gh-pages
+@echo off
+
+IF "%~1" == "" GOTO fail
+
+call npm run build
+call git add --all
+call git commit -m %1
+call git push origin master
+call git subtree push --prefix build origin gh-pages
+echo "Done"
+goto:eof
+
+:fail
+    echo "Commit msg not specified"
